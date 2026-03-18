68993092ed31297b0ff4f85f839ed2c8818c8842
JoJaehyeok <jjhstc2285@naver.com>
2026-03-18 22:54:19 +0900

    dsadsada

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // 이 줄이 있어야 /api/v1/... 구조가 먹힙니다.
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
